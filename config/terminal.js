26af57f247ef4ab1b4fa0b2b87b0325c10fdb091
PineberryCode <applepie090902@gmail.com>
2023-07-28 21:35:31 -0500

    c: testing it

---
+++ config/terminal.js
@@ -0,0 +1,13 @@
+const outputElement = document.getElementById("output");
+
+let frame = 0;
+let frame02 = 0;
+
+const frames = [" ******* ","    *******    ", "       *******       "];
+
+function updateDonut () {
+    outputElement.textContent = frames[frame];
+    frame = (frame + 1) % frames.length;
+}
+
+setInterval(updateDonut, 200);
