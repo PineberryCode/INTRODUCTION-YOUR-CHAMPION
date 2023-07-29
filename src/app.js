d56f2e128d5c638fa94b23633d6c2b2b11736beb
MINDLUNNY <88601032+PineberryCode@users.noreply.github.com>
2023-07-28 19:29:18 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,5 @@
-
-const express = require('express');
-const server = require('./server/config');
-
-const _app = server(express());
-
-_app.listen(_app.get('port'), () => {
-    console.log('Server running on port', _app.get('port'));
-})
+<head>    
+</head>
+<body>
+    chao    
+</body>
